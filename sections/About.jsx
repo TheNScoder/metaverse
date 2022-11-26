698418c3417ea2c7c64e46093bb68e120241cc69
thenscoder <sujanneupane17@gmail.com>
2022-11-26 11:18:39 +0545

    explore section with images display slightly finished

--- sections/About.jsx
+++ sections/About.jsx
@@ -14,7 +14,7 @@ const About = () => (
       viewport={{ once: false, amount: 0.5 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="|About Metaverse" textStyles="text-center" />
+      <TypingText title="| About Metaverse" textStyles="text-center" />
       <motion.p variants={fadeIn("up", "tween", 0.2, 1)}
       className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center
       text-secondary-white"
